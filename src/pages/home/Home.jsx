b33e50d963007400dfb2fdc2419405debd6c68e7
RobertaDuprat <robertaduprat@gmail.com>
2023-11-01 10:43:07 -0300

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './home.scss';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className="homeContainer">
+        <h1>Firefighters-sre</h1>
+        </div> 
+    <div className='background'>
+    </div>
+    </div>
+   
+  )
+}
+
+export default Home
