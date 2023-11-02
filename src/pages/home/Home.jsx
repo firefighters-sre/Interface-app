13389e26234476c3ffc5daa6259dd586560f7c9f
RobertaDuprat <robertaduprat@gmail.com>
2023-11-01 22:42:31 -0300

    Interface base was add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './home.scss';
+import ListaEvento from './ListaEvento.jsx';
 
 const Home = () => {
   return (
@@ -7,8 +8,14 @@ const Home = () => {
       <div className="homeContainer">
         <h1>Firefighters-sre</h1>
         </div> 
-    <div className='background'>
-    </div>
+      <div className="lista">
+      < ListaEvento />
+      </div>
+      <div className='status'>
+        <h2>Off</h2>
+      </div>
+     
+  
     </div>
    
   )
---
+++ src/pages/home/ListaEvento.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+function RoomLog() {
+  const [log, setLog] = useState([]);
+  const maxLines = 3;
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setLog((prevLog) => {
+        const newLog = [`Usuário ${new Date().getTime()} entrou na sala`, ...prevLog];
+        if (newLog.length > maxLines) {
+          newLog.pop(); // Remove a linha mais antiga
+        }
+        return newLog;
+      });
+    }, 10000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div>
+      {log.map((entry, index) => (
+        <p key={index}>{entry}</p>
+      ))}
+    </div>
+  );
+}
+
+export default RoomLog;
