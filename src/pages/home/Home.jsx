e773e02df9cfed363fee69eb3801030b7c8f6872
RobertaDuprat <robertaduprat@gmail.com>
2023-11-03 11:59:30 -0300

    First interface is ready!!!

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './home.scss';
 import ListaEvento from './ListaEvento.jsx';
+import elevadordesativado from './elevador-desativado.png';
 
 const Home = () => {
   return (
@@ -13,9 +14,11 @@ const Home = () => {
       </div>
        <div className='status'>
       <h2>Off</h2>
+    
+      </div> 
+       <div>
+      <img src={elevadordesativado} className='elevadordesativado'/>
       </div>
-     
-  
     </div>
    
   )
