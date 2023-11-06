10a1a2269de92af9ccc625fbffcee1c315d187ea
RobertaDuprat <robertaduprat@gmail.com>
2023-11-06 10:16:51 -0300

    correção de bug

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./home.scss";
 import ListaEvento from "./ListaEvento.jsx";
 import elevadordesativado from "./elevador-desativado.png";
@@ -6,6 +6,7 @@ import elevadorligado from "./elevador-ativado.png";
 import elevadoremchamas from "./elevador-em-chamas.png";
 import caboscortados from "./cabos-cortados.png";
 import cabosconectados from "./cabos-conectados.png";
+import problema from "./problema.png";
 
 const Home = () => {
   const [status, setStatus] = useState("Off");
@@ -16,11 +17,14 @@ const Home = () => {
   const [valor1] = useState();
   const [valor2] = useState();
   const [metricasVisiveis, setMetricasVisiveis] = useState(false);
+  const [iconeProblemaVisivel, setIconeProblemaVisivel] = useState(false);
+  const [novoPopupVisivel, setNovoPopupVisivel] = useState(false);
 
   const toggleStatus = () => {
     if (botaoAtivo) {
       setStatus("On");
       setBotaoAtivo(false);
+      
     }
   };
 
@@ -37,27 +41,36 @@ const Home = () => {
     setPopupVisible(true);
     setListaEventosVisible(false);
     setBotaoAtivo(false); // Oculte permanentemente o botão "ON"
+    setMetricasVisiveis(false);
+    setIconeProblemaVisivel(true);
   };
 
   const togglePopup = () => {
     setPopupVisible(!popupVisible);
+    setMetricasVisiveis(true); // Exibe as métricas quando o popup é fechado
+    setIconeProblemaVisivel(true);
 
     if (popupVisible) {
       // Se o popup foi fechado, reexiba a lista de eventos
       setListaEventosVisible(false);
-     
-      setMetricasVisiveis(true); 
-
     }
   };
-  
+
+  useEffect(() => {
+    if (popupVisible) {
+      setMetricasVisiveis(false);
+      setIconeProblemaVisivel(false);
+      setListaEventosVisible(false);
+      setNovoPopupVisivel(false);
+    }
+  }, [popupVisible]);
 
   return (
     <div className="home">
       <div className="homeFContainer">
         <h1>Firefighters-sre</h1>
       </div>
-      {listaEventosVisible && ( // Renderize a lista de eventos apenas se ela estiver visível
+      {listaEventosVisible && (
         <div className="listaevento">
           <ListaEvento />
         </div>
@@ -97,19 +110,30 @@ const Home = () => {
             </button>
           </div>
         )}
-      {metricasVisiveis && (
-  <div className="metricas">
-    <div className="valor-box">
-      <span>Tempo: 50%</span>
-      <span>{valor1}</span>
-    </div>
-    <div className="valor-box">
-      <span>Disponibilidade: 60% </span>
-      <span>{valor2}</span>
-    </div>
-  </div>
-)}
-
+        {metricasVisiveis && (
+          <div className="metricas">
+            <div className="valor-box">
+              <span>Tempo: 50%</span>
+              <span>{valor1}</span>
+            </div>
+            <div className="valor-box">
+              <span>Disponibilidade: 60%</span>
+              <span>{valor2}</span>
+            </div>
+          </div>
+        )}
+        {iconeProblemaVisivel && (
+          <img
+            src={problema}
+            className="problema"
+            onClick={() => setNovoPopupVisivel(true)}
+          />
+        )}
+        {novoPopupVisivel && (
+          <div className="novopopup">
+        
+          </div>
+        )}
       </div>
     </div>
   );
