cabb8cfbfc5a919ccd6fa8516f462015cde1b39c
Gabriel Sampaio <gsampaio@redhat.com>
2023-11-13 10:06:53 -0300

    small fixes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,32 +1,32 @@
 import React, { useState } from 'react';
-import './home.scss';
 import StatusButton from '../../components/StatusButton/StatusButton';
 import Elevator from '../../components/Elevator/Elevator';
+import './home.scss';
 
 const Home = () => {
   const [status, setStatus] = useState('Off');
   const [botaoAtivo, setBotaoAtivo] = useState(true);
+
   const toggleStatus = () => {
     setStatus(currentStatus => currentStatus === 'Off' ? 'On' : 'Off');
-    setBotaoAtivo(currentActive => !currentActive);
+    setBotaoAtivo(!botaoAtivo);
   };
 
   return (
-    <div className="flex flex-col items-center justify-between min-h-screen text-center font-sans p-4">
-
-      <div className="mt-8">
+    <main className="flex flex-col items-center justify-between min-h-screen text-center p-4">
+      <header className="mt-8">
         <h1 className="text-4xl font-bold text-gray-800">Firefighters-sre</h1>
-      </div>
+      </header>
       
-      <div className="mb-30">
+      <section className="mb-20">
         <StatusButton isActive={botaoAtivo} status={status} toggleStatus={toggleStatus} />
-      </div>
+      </section>
 
       <div className="predio">
         <Elevator status={status} />
       </div>
       
-    </div>
+    </main>
   );
 };
 
